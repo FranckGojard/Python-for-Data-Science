fa62daedf77255aef69c5dcbc2494408d7e73031
Franck GOJARD <frgojard@paul-f4Ar4s5.clusters.42paris.fr>
2025-03-13 13:08:19 +0100

    day01

---
+++ Python-1-Array/ex00/give_bmi.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+
+def give_bmi(height: list[int | float], weight: list[int | float]) -> list[int | float]:
+    heights = np.array(height)
+    weights = np.array(weight)
+    bmi = [weights / (heights ** 2)]
+    return (bmi)
+
+
+def apply_limit(bmi: list[int | float], limit: int) -> list[bool]:
+    lst_limit = []
+    for 
+    return lst_limit
